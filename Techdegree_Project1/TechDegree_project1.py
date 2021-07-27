69600994578e569c7f414dba835cc19dd6d02fc4
jlgardner256 <74513286+jlgardner256@users.noreply.github.com>
2021-07-26 19:00:37 -0500

    Update TechDegree_project1.py

--- Techdegree_Project1/TechDegree_project1.py
+++ Techdegree_Project1/TechDegree_project1.py
@@ -10,40 +10,40 @@ def start_game():
     choice_1 = input("Hello, {} welcome to the number guessing game!".format(name))
     guesses = 1
    
-    random = random.randint(1, 10)
+    randoms = random.randint(1, 10)
     
     #random = random.randint(1, 10)
-    
-    while True:
+    play = True
+    while play:
         
         try:
-            choice_2 = input("Pick a number Between 1 and 10!  ")
-            choice_2 = int(choice_2)
-        
+            choice_2 = int(input("Pick a number Between 1 and 10!  "))
+                    
         except ValueError as wrong_value:
             print("oops! you can only use a number for a guess.")
             continue
        
                
-        if choice_2 > random:
+        if choice_2 > randoms:
             print( " Uh oh, the number your looking for is lower")
             guesses += 1
             continue
             
-        if choice_2 < random:
+        elif choice_2 < randoms:
             print("uh oh, the number your looking for is higher")
             guesses += 1
             continue
-            
-        if choice_2 == random:
-            print (" You guessed the right number congrats!")
+       
+        else:
+            print(" You guessed the right number congrats!")
             print(" It took you {} times to find the right number!".format(guesses))
             choice_3 = input(" Would you like to play again?\n Yes/No  ")
-           
+
             if choice_3.lower() == "yes":
-                return start_game ()
+                return start_game()
             else:
-                break
+                play = False
+        
 
                 
 start_game()
